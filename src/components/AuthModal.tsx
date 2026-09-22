a486b04fef9efa21d0efc2011a19b72a36ee1edb
hyxxniz <hyxxniz@naver.com>
2026-09-22 15:35:03 +0900

    fix: K-water 물문화관 공식 로고 해상도 규격 800x250px 리사이징 적용

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -230,8 +230,8 @@ export function AuthModal() {
           <Image
             src="/images/kwater_waterhub_logo.png"
             alt="K-water 한국수자원공사 물문화관"
-            width={828}
-            height={276}
+            width={800}
+            height={250}
             className="h-12 sm:h-13 w-auto object-contain"
             priority
           />
